e32efd4cf1f011909c2c11af2baaba1043dc3821
Bryan Yapdhika <bryanyapdhika231@gmail.com>
2024-03-03 18:23:18 +0700

    fixed

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -16,11 +16,20 @@ st.title("Polusi di distrik Beijing")
 # Dataset
 df_airquality = pd.read_csv("https://raw.githubusercontent.com/FOLZi-4G/Analisis-Data-Dicoding/main/dashboard/main_data.csv")
 
-# Nambah Kolom tanggal.
-date_columns = ['year', 'month', 'day', 'hour']
-df_airquality["date"] = pd.to_datetime(df_airquality[date_columns])
-df_airquality.insert(0, 'date', df_airquality.pop('date'))
-# Seakrang data tanggal sudah dibuat maka 4 kolom tanggal bisa dihapuskan
-df_airquality.drop(date_columns, axis=1, inplace=True)
+# Min dan Max data
+min_date = df_airquality["date"].min().date()
+max_date = df_airquality["date"].max().date()
 
-df_airquality.info()
+with st.sidebar:
+    # add Text
+    st.header("Air Quality")
+    # add logo
+    st.image("https://st2.depositphotos.com/4520249/7571/v/950/depositphotos_75718829-stock-illustration-blowing-wind-icon.jpg")
+    
+    # Get start_date & end_date from date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
